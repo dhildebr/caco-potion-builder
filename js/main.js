c96d2d49c6faf6a97bd052432087308f84611981
Daniel Hildebrandt <daniel.s.hildebrandt@gmail.com>
2019-09-01 17:35:56 -0700

    Intermediate commit: need to call JSON data remotely, cannot access local copy without user input.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function loadIngredientJsonData(path, callback)
+{
+  let xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open("GET", ingredientDataPath, true);
+  
+  xobj.onreadystatechange = function() {
+    if(xobj.readyState == 4 && xobj.status == "200")
+      callback(xobj.responseText)
+  };
+  
+  xobj.send(null);
+}
+
+const ingredientDataPath = "../json/caco-ingredients.json";
+let ingredientDataJson = null;
+loadIngredientJsonData(function(response) {
+  ingredientDataJson = JSON.parse(response);
+});
+
+console.log(ingredientDataJson);
